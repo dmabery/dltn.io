19620f2c3ab2cd4738dfab50577930e90fbc6fbc
daltonmabery <maberydalton@gmail.com>
2022-02-18 13:48:48 -0800

    add book

--- components/SideNote.js
+++ components/SideNote.js
@@ -5,7 +5,7 @@ const SideNote = (props) => {
         <div className="rounded-sm bg-gray-700 p-4 border-l-4 border-amber-700 prose mt-4">
             <div className="flex">
             <div className="flex-shrink-0 mt-8 mr-2">
-                <Image src="/astronaut waving.png" height={50} width={50}  />
+                <Image src="/astronaut waving.png" height={60} width={50}  />
                 </div>
             <div className="ml-3 pr-10">
                 <p className="text-lg mb-3 font-bold text-slate-100">{props.title}</p>
