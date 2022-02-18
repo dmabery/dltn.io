9261a96deca00c112913dc1035cad3f389234719
daltonmabery <maberydalton@gmail.com>
2022-02-18 13:35:33 -0800

    update sidenote

--- components/SideNote.js
+++ components/SideNote.js
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 const SideNote = (props) => {
     return (
-        <div class="rounded-sm bg-gray-700 p-4 border-l-4 border-amber-700 prose mt-4">
+        <div className="rounded-sm bg-gray-700 p-4 border-l-4 border-amber-700 prose mt-4">
             <div className="flex">
-            <div class="flex-shrink-0 mt-8 mr-2">
+            <div className="flex-shrink-0 mt-8 mr-2">
                 <Image src="/astronaut waving.png" height={50} width={50}  />
                 </div>
-            <div class="ml-3 pr-10">
+            <div className="ml-3 pr-10">
                 <p className="text-lg mb-3 font-bold text-slate-100">{props.title}</p>
-                <p class="text-slate-100">{props.content}</p>
+                <p className="text-slate-100">{props.content}</p>
             </div>
         </div>
         </div>
