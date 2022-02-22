f14169253422814603f578df96bb07cf445991bf
daltonmabery <maberydalton@gmail.com>
2022-02-22 15:54:31 -0800

    add margin on index list of posts

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home({ posts }) {
                 <h3 className="text-3xl text-gray-900 font-bold">Latest posts</h3>
                   <div className="mt-5">
                     {posts.slice(0, 5).map((post, index) => (
-                      <div className="max-w-lg text-slate-500" key={index}>
+                      <div className="max-w-lg text-slate-500 mb-1" key={index}>
                         <Link href={'/essays/' + post.slug} passHref>
                               <span className="col-span-3 text-l underline decoration-slate-500 hover:decoration-amber-700 leading-6 font-bold hover:decoration-2">{post.frontMatter.title}</span>
                         </Link>
