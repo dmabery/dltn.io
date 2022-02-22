86d3df71685bdd1bdaa7f09ba418e6c6724f9c84
daltonmabery <maberydalton@gmail.com>
2022-02-22 15:44:56 -0800

    add post list component

--- pages/essays.js
+++ pages/essays.js
@@ -4,6 +4,7 @@ import Meta from '../components/Meta'
 import PageTitle from '../components/PageTitle'
 import PageContent from '../components/Layout'
 import filterPostsByType from '../lib/filterPostsByType'
+import PostList from '../components/PostList'
 
 export const getStaticProps = async () => {
   return filterPostsByType('blog');
@@ -18,25 +19,9 @@ const Essays = ({ posts }) => {
         description="Most of my essays are expanding on an idea I read in a book or heard in a podcast. These essays are a record of my becoming of a person while trying to answer the question, 'What does this all mean?'"      />
 
       <PageContent>
-
-        <div className="grid grid-row gap-4">
+      <div className="grid grid-row gap-4">
             {posts.map((post, index) => (
-                <div className="rounded-lg mb-4" key={index}>
-                <div className="" key={index}>
-                    <Link href={'/essays/' + post.slug} passHref key={index}>
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900">{post.frontMatter.title}</h5>
-                    </Link>
-                    <p className="mb-2 font-normal text-gray-400">{post.frontMatter.description}</p>
-                    <p className="mb-4 text-sm text-gray-400">Published on: {post.frontMatter.date}</p>
-                    <Link href={'/essays/' + post.slug} passHref key={index} class="inline-flex items-center text-sm font-medium text-center text-slate-500 hover:text-slate-900">
-                        <div className="text-gray-700 flex flex-column items-center">
-                          Read more
-                          <svg className="ml-2 -mr-1 w-4 h-4 hover:ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                        </div>
-                    </Link>
-                </div>
-            </div>
-
+              <PostList post={post} index={index} />
             ))}
         </div>
         </PageContent>
--- pages/notes.js
+++ pages/notes.js
@@ -4,6 +4,7 @@ import Meta from '../components/Meta'
 import PageTitle from '../components/PageTitle'
 import PageContent from '../components/Layout'
 import filterPostsByType from '../lib/filterPostsByType'
+import PostList from '../components/PostList'
 
 export const getStaticProps = async () => {
   return filterPostsByType('notes');
@@ -20,25 +21,10 @@ const NoteList = ({ posts }) => {
       <PageContent>
         <div className="grid grid-row gap-4">
             {posts.map((post, index) => (
-                <div className="rounded-lg dark:border-gray-700 mb-4 relative" key={index}>
-                <div>
-                    <Link href={'/essays/' + post.slug} passHref key={index}>
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900">{post.frontMatter.title}</h5>
-                    </Link>
-                    <p className="mb-4 font-normal text-gray-700 dark:text-gray-400">{post.frontMatter.description}</p>
-                    <Link href={'/essays/' + post.slug} passHref key={index} class="inline-flex items-center text-sm font-medium text-center text-slate-500 hover:text-slate-900">
-                        <div className="text-gray-700 flex flex-column items-center">
-                          Read more
-                          <svg className="ml-2 -mr-1 w-4 h-4 hover:ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                        </div>
-                    </Link >
-                </div>
-            </div>
-
+              <PostList post={post} index={index} />
             ))}
         </div>
       </PageContent>
-
     </>
     )
 }
