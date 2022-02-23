99f4f2bc3e724adb0a002cbdfdde7bfa284c4c6b
daltonmabery <maberydalton@gmail.com>
2022-02-23 14:40:47 -0800

    add gif to PageTitle component

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,8 +1,9 @@
 import Link from 'next/link'
+import Image from 'next/image';
 
 const PageTitle = ({title, description}) => {
     return (
-        <div className="flex flex-row">
+        <div className="flex flex-row max-h-60 content-center">
             <div className="basis-2/3">
                 <h2 className="text-[50px] text-gray-900 font-bold"><span className="underline decoration-amber-700">{title}</span></h2>
                 {description.length > 0 &&
@@ -10,7 +11,10 @@ const PageTitle = ({title, description}) => {
                 }
                 <br />
             </div>
-      </div>
+            <div className="ml-2">
+             <Image src='/ezgif.com-gif-maker.gif' width={175} height={175} />
+            </div>
+        </div>
     )
 }
 
