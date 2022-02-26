698e4d16c413068872dde77560a47daf30a3f166
daltonmabery <maberydalton@gmail.com>
2022-02-26 12:19:19 -0800

    adjust h1 sizing on essay template

--- pages/essays/[slug].js
+++ pages/essays/[slug].js
@@ -54,7 +54,7 @@ export const getStaticPaths = async () => {
       return (
         <>
         <Meta title={title} description={description} image={thumbnailUrl} />
-          <div className="container mx-auto max-w-screen-sm prose-white prose-a:text-sky-700 hover:prose-a:text-amber-700 focus:prose-a:text-gray-900 prose prose-a:no-underline hover:prose-a:underline prose-h1:leading-tight pr-5 pl-5 md:p-0">
+          <div className="container mx-auto max-w-screen-sm prose-white prose-a:text-sky-700 hover:prose-a:text-amber-700 focus:prose-a:text-gray-900 prose prose-a:no-underline hover:prose-a:underline prose-h1:leading-tight pr-5 pl-5 md:p-0 prose-h1:text-[33px] sm:prose-h1:text-5xl" >
             <div className="border-b-2 pb-3 mb-10">
               <h1 className="text-left text-5xl mt-7 mb-4 underline decoration-amber-700 decoration-4">{title}</h1>
               <div className="text-md">{description}</div>
