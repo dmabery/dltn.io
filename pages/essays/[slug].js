e18a279c92988c3c47914748146083d93566e7cb
daltonmabery <maberydalton@gmail.com>
2022-04-15 19:41:45 -0700

    Update [slug].js

--- pages/essays/[slug].js
+++ pages/essays/[slug].js
@@ -41,12 +41,11 @@ export const getStaticProps = async ({params}) => {
 }
 
   const PostPage = ({post}) => {
-    console.log(post)
     if (!post) return <div>loading</div>
   
       return (
       <>
-                <PostTitleDisplay title={post.fields.title} date={post.sys.createdAt} content={post.fields.content2} image={post.fields.image.fields.file.url}/>
+                <PostTitleDisplay title={post.fields.title} date={post.sys.createdAt} content={post.fields.content2} image={post.fields.image.fields.file.url || ''}/>
         
               </>   
       )
