52c73445e6fb6e7e685e5ab4026e0e3da2482501
daltonmabery <maberydalton@gmail.com>
2022-05-21 08:47:36 -0700

    add OG:Image defaults

--- pages/essays/[slug].js
+++ pages/essays/[slug].js
@@ -41,9 +41,10 @@ export const getStaticProps = async ({params}) => {
 
   const PostPage = ({post}) => {
     if (!post) return <div>loading</div>
+    const checkImage = post.fields.hasOwnProperty('image') ? `https://` + post.fields.image.fields.file.url : 'https://images.ctfassets.net/nk2hkdvz2uym/5yB87OQ11Ps4rMfWMQay'
       return (
       <>
-                <PostTitleDisplay title={post.fields.title} description={post.fields.description} date={post.sys.createdAt} content={post.fields.content2} image={`https:` + post.fields.image.fields.file.url || 'https://images.ctfassets.net/nk2hkdvz2uym/5yB87OQ11Ps4rMfWMQay'}/>
+                <PostTitleDisplay title={post.fields.title} description={post.fields.description} date={post.sys.createdAt} content={post.fields.content2} image={checkImage}/>
         
               </>   
       )
