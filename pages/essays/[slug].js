ff0f5eda8863287adbe76ec69df3a6c88c4b9179
daltonmabery <maberydalton@gmail.com>
2022-05-21 08:21:35 -0700

    Add OG:Image to Posts

--- pages/essays/[slug].js
+++ pages/essays/[slug].js
@@ -43,7 +43,7 @@ export const getStaticProps = async ({params}) => {
     if (!post) return <div>loading</div>
       return (
       <>
-                <PostTitleDisplay title={post.fields.title} description={post.fields.description} date={post.sys.createdAt} content={post.fields.content2}/>
+                <PostTitleDisplay title={post.fields.title} description={post.fields.description} date={post.sys.createdAt} content={post.fields.content2} image={`https:` + post.fields.image.fields.file.url}/>
         
               </>   
       )
