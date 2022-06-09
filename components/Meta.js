87e1edfec331a30f600c10571af7141e34921af6
daltonmabery <maberydalton@gmail.com>
2022-06-08 21:34:26 -0700

    add og Image tags

--- components/Meta.js
+++ components/Meta.js
@@ -5,16 +5,14 @@ const Meta = (props) => {
         <>
             <Head>
                 <title>{props.title}</title>
-                <meta name="description" content={props.description} />
-                <meta property="og:title" content={props.title}/>
                 <meta property="og:site_name" content="dalton's site" />
-                <meta property="og:description" content="Understanding more of the world."/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:url" content="https://daltonmabery.com/"/>
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:creator" content="@daltonmabery" />
-                <meta property="og:title" content={props.title} />
-                <meta property="og:description" content={props.description} />
+                <meta property="og:title" content={props.title}/>
+                <meta property="og:description" content="Understanding more of the world."/>
+                <meta name="description" content={props.description} />
                 <meta property="og:image" content={props.image} />
             </Head>
         </>
