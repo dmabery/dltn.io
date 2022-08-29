118a9773c5bf4ca59288e699a6b03b153d585e41
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2022-08-28 17:47:22 -0700

    update about page copy

--- components/PostList.js
+++ components/PostList.js
@@ -1,27 +1,39 @@
 import Link from 'next/link';
 import TagButton from './TagButton';
 
-const PostList = ({ key, date, slug, description, title }) => {
+const PostList = ({ key, date, slug, description, title, contentType }) => {
+    function checkContentType() {
+        if (contentType === 'tinyThought') {
+            return {slug: 'thoughts/', tag: 'thought'};
+        } else return {slug: 'essays/', tag: 'essay'};
+    }
+
     return (
         <div>
             <div className="transition-all mb-10 rounded md:mr-5 mr-2" key={key}>
                 <div>
-                    <div className="flex flex-col justify-between md:flex-row md:justify-between">
-                        <div>
-                            <TagButton />
-                            <Link href={`/essays/${slug}`} passHref>
+                    <div className="flex flex-col justify-between md:flex-row">
+                        <div className='flex md:items-center md:flex-row flex-col'>
+                            <Link href={checkContentType().slug + slug} passHref>
                                 <a className="text-xl font-bold text-neutral-100 md:mb-0 mb-1 hover:text-amber-200 hover:underline">
                                     {title}
                                 </a>
                             </Link>
                         </div>
-                        <p className="text-xs text-neutral-300">{date.slice(0, 10)}</p>
+                        <div>
+                         <p className="text-xs text-neutral-300">{date.slice(0, 10)}</p>
+                         <div>
+                                <TagButton
+                                    btnColor={checkContentType().tag === 'thought' ? 'bg-red-600' : ''}
+                                    btnText ={checkContentType().tag === 'thought' ? 'Aside' : 'Essay'}
+                                />
+                            </div>
+                        </div>
                     </div>
                     <p className="mt-2 mb-2 font-normal text-neutral-300">{description}</p>
                 </div>
             </div>
         </div>
     );
-};
-
+    }
 export default PostList;
--- pages/about.js
+++ pages/about.js
@@ -12,27 +12,41 @@ const AboutMe = () => {
             />
 
             <PageContent>
-                <div className="prose max-w-screen-lg prose-invert font-light prose-strong:text-neutral-100 text-neutral-300 prose-a:no-underline prose-h1:leading-tight prose-h3:text-slate-300 prose-h4:text-slate-300 sm:prose-h1:leading-tight md:p-0 prose-blockquote:border-none prose-blockquote:font-normal prose-blockquote:bg-slate-400/40 prose-blockquote:rounded prose-blockquote:leading-normal prose-blockquote:p-1 prose-blockquote:pl-5 prose-blockquote:pr-5 prose-blockquote:not-italic">
-                    <p>My name is Dalton Mabery -- a video editor, developer, reader, writer, thinker, and lover of the curious things in the world. I try to understand how things work through the essential inner dialogue that populates this site and <a className="text-sky-800 underline decoration-amber-700 decoration-2" href="https://twitter.com/daltonmabery">my Twitter</a>. This is a record of my becoming of a person as I explore the intellectual, creative, philosophical, and spiritual questions in this world through extensive research of literature -- both young and old. A private journey made public in seek of the one true question we all ask at some point: <strong><em>who am I and what is all this?</em></strong></p>
-                    <p>Whenever I can refer to wisdom from those who lived before me, I do. The following quote comes from Will Durant in <em>The Map of Human Character </em>and was the first time I was able to assign words to the inner motivation that drives me -- <strong>how can we best make sense of everything in the world?</strong></p>
-                    <blockquote>
-                    <p>Therefore I feel that we of this generation give too much time to news about the transient present, too little to the living past. <strong>We are choked with news, and starved of history.</strong> We know a thousand items about the day or yesterday, we learn the events and troubles and heartbreaks of a hundred peoples, the policies and pretensions of a dozen capitals, the victories and defeats of causes, armies, athletic teams. <br /> <br />
-                    
-                    But how, without history, can we understand these events, discriminate their significance, sift out the large from the small, see the basic currents underlying surface movements and changes, and foresee the result sufficiently to guard against fatal error or the souring of unreasonable hopes?&quot; <br /> <br />
-                    
-                    - Will Durant, &quot;The Map of Human Character</p>
-                    </blockquote>
-                    <p> The legacy I hope to leave behind is similar to that of Albert Einstein&#39;s. Not his groundbreaking theroies, but his curiosity and humility. After his death, <i>The New York Times</i> wrote this:</p>
-                    <blockquote>
-                        <p>Man stands on this diminutive earth, gazes at the myriad of stars and upon billowing ocean and tossing trees–and wonders. What does it all mean? How did it come about? The most thoughtful wonderer who appeared among us in three centuries has passed on in the person of Albert Einstein.</p>
-                    </blockquote>
-                    <p>&quot;The most thoughtful wonderer.&quot; I love that. I hope those words are used to describe me one day.</p>
-                    <hr />
-                    <h2>Career</h2>
-                    <p>On a professional note, I have a degree in Digital Media &amp; Design from Southeastern University and work as a Digital Creator and Designer for <a className="text-sky-800 underline decoration-amber-700 decoration-2" href="https://fs.blog/">Farnam Street</a>.</p>
-                    <p>Developer - I built this site using Tailwind and React. I&#39;d like my next job to be as a programmer.</p>
+                <div className="prose max-w-screen-md prose:ml-0 underline-offset-2 prose-strong:text-neutral-100 text-neutral-300 prose-a:text-amber-500 prose-a:no-underline hover:prose-a:underline hover:prose-a:text-amber-400 prose-h1:leading-tight prose-h2:text-amber-300 prose-h3:text-neutral-100 prose-h4:text-neutral-100 sm:prose-h1:leading-tight prose-blockquote:font-normal prose-blockquote:text-neutral-300 prose-blockquote:border-red-600 prose-blockquote:leading-normal prose-blockquote:pl-5 prose-blockquote:not-italic">
+
+                
+            <h2 id="tldr">TLDR</h2>
+                <p>Greetings, I&#39;m Dalton! I&#39;m a professional video editor with four years of social media experience and have a degree in Digital Media and Design, currently working for <a href="https://fs.blog/">Farnam Street </a>. I also <a href="https://www.daltonmabery.com/writing">write online</a> and share interesting quotes and ideas on <a href="https://twitter.com/daltonmabery">Twitter</a>.</p>
+                <h2 id="what-i-m-doing-now">What I&#39;m Doing Now</h2>
+                <h3 id="for-work">For work</h3>
+                <ul>
+                <li>I currently work for <a href="https://fs.blog/">Farnam Street</a>. I spend most of my time operating and growing our <a href="https://www.youtube.com/channel/UCLtTf_uKt0Itd0NG7txrwXA">podcast&#39;s YouTube channel</a> (we hit 100k subscribers in August of 2022). When I&#39;m not doing that, I work on <a href="https://www.youtube.com/watch?v=k_zt6qRTmN0">creating engaging scripts and videos</a> from our extensive blog content. I also run social accounts for <a href="https://twitter.com/farnamstreet">Farnam Street</a> and <a href="https://www.instagram.com/tkppodcast/">The Knowledge Project</a>. </li>
+                </ul>
+                <h3 id="for-side-projects">For Side projects</h3>
+                <ul>
+                <li>When I&#39;m not working, I&#39;m either reading <a href="https://www.daltonmabery.com/notes">an interesting book</a> or <a href="https://github.com/dmabery">coding</a>. I&#39;m studying to become a software engineer, so I&#39;m spending lots of time building small tools and watching new tutorials. I try to get 1% better every day.</li>
+                </ul>
+                <h2 id="for-fun">For Fun</h2>
+                <ul>
+                <li>I enjoy working more than most, but when I do sneak away from my computer, it&#39;s either to go to the gym, play golf (my goal is to break 80 this year), watch golf, or hang out with friends and famuily. My main leisure activity is golf so if you play, let&#39;s hit the links!</li>
+                </ul>
+                <h2 id="extended-about">Extended About</h2>
+                <p>I try to understand how things work through the essential inner dialogue that populates this site and <a href="https://twitter.com/daltonmabery">my Twitter</a>. This is a record of my becoming of a person as I explore the intellectual, creative, philosophical, and spiritual questions in this world through extensive research of literature -- both young and old. A private journey made public in seek of the one true question we all ask at some point: <strong><em>who am I and what is all this?</em></strong></p>
+                <p>Whenever I can refer to wisdom from those who lived before me, I do. The following quote comes from Will Durant in <em>The Map of Human Character</em> and was the first time I was able to assign words to the inner motivation that drives me -- <strong>how can we best make sense of everything in the world?</strong></p>
+                <blockquote>
+                <p>Therefore I feel that we of this generation give too much time to news about the transient present, too little to the living past. <strong>We are choked with news, and starved of history.</strong> We know a thousand items about the day or yesterday, we learn the events and troubles and heartbreaks of a hundred peoples, the policies and pretensions of a dozen capitals, the victories and defeats of causes, armies, athletic teams.  </p>
+                <p>But how, without history, can we understand these events, discriminate their significance, sift out the large from the small, see the basic currents underlying surface movements and changes, and foresee the result sufficiently to guard against fatal error or the souring of unreasonable hopes?&quot;  </p>
+                <p>  Will Durant, &quot;The Map of Human Character</p>
+                </blockquote>
+                <p>The legacy I hope to leave behind is similar to that of Albert Einstein&#39;s. Not his groundbreaking theroies, but his curiosity and humility. After his death, <em>The New York Times</em> wrote this:</p>
+                <blockquote>
+                <p>Man stands on this diminutive earth, gazes at the myriad of stars and upon billowing ocean and tossing trees–and wonders. What does it all mean? How did it come about? The most thoughtful wonderer who appeared among us in three centuries has passed on in the person of Albert Einstein.</p>
+                </blockquote>
+                <p>&quot;The most thoughtful wonderer.&quot; I love that. I hope those words are used to describe me one day.</p>
+                <h3 id="socials">Socials</h3>
+                <p>You can find me on <a href="https://www.twitter.com/daltonmabery">Twitter</a>, <a href="https://github.com/dmabery">GitHub</a>, and <a href="https://www.youtube.com/daltonmabery">YouTube</a>.</p>
                 </div>
-            </PageContent>
+        </PageContent>
         </>
     )
 }
