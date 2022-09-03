ba9c64dd30cfc7cd1c406db3333aaf963834e16f
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2022-09-03 12:23:40 -0700

    add mb-5 to sidenote

--- components/SideNote.js
+++ components/SideNote.js
@@ -1,8 +1,7 @@
-import Image from 'next/image';
 
 const SideNote = (props) => {
     return (
-        <div className="mt-4">
+        <div className="mt-4 mb-5">
             <div className="text-md translate-y-6 font-mono font-bold text-slate-900 bg-slate-100 inline px-3 py-2 rounded-md ml-5 translate-y-6">{props.title}</div>
             <div className="-mt-1 text-slate-100 text-md font-mono bg-slate-700 pl-6 rounded py-3 border border-blue-300">{props.content}</div>
         </div>
