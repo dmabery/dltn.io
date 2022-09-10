55e163f9500aaeaf9de021ec77253c5b230ab12c
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2022-09-10 13:28:32 -0700

    add til description

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { Fragment } from 'react';
 
 function PageTitle({ title, description, image, width, height, small }) {
   const condition = image ? 'flex' : 'flex-none sm:flex';
@@ -6,11 +7,13 @@ function PageTitle({ title, description, image, width, height, small }) {
 
   return (
     <div className={`flex-row items-center ${condition}`}>
-      <div className="basis-2/3">
+      <div className="w-3/4">
         <div className={`${secondary} text-slate-300 font-bold mb-2 font-mono`}>
           {title}
         </div>
-        <p className="text-slate-300">{description}</p>
+        <div className="prose max-w-screen-md prose:ml-0 underline-offset-2 prose-strong:text-neutral-100 text-neutral-300 prose-a:text-blue-400 prose-a:no-underline hover:prose-a:underline hover:prose-a:text-amber-400 prose-h1:leading-tight prose-h2:text-yellow-300 prose-h3:text-neutral-100 prose-h4:text-neutral-100 sm:prose-h1:leading-tight prose-blockquote:font-normal prose-blockquote:text-neutral-300 prose-blockquote:border-red-500 prose-blockquote:leading-normal prose-blockquote:pl-5 prose-blockquote:not-italic">
+          <Fragment>{description}</Fragment>
+        </div>
       </div>
       {image ? (
         <div className="ml-2">
