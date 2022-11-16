4b271e34d3dc0386dbe4df93712a09547139cc9c
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2022-11-15 16:35:38 -0800

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-script-in-head */
+import { Analytics } from '@vercel/analytics/react';
+
 import Head from 'next/head';
-import Script from 'next/script';
 import "prismjs";
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
@@ -12,16 +13,6 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" href="/favicon 3.png" />
-        <Script strategy="lazyOnLoad" id="ga-script" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
-        <Script strategy="lazyOnLoad" id="ga-script">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-            `}
-          </Script>
       </Head>
 
       <div className="container px-5 mx-auto max-w-screen-md">
@@ -30,6 +21,7 @@ function MyApp({ Component, pageProps }) {
           </nav>
           <main className="mb-auto min-h-screen">
               <Component {...pageProps} />
+              <Analytics />
           </main>
           <footer className="h-10">
             <Footer />
