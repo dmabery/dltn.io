82e280626c9c121da1b404601d57f51715e15978
daltonmabery <maberydalton@gmail.com>
2023-01-23 06:51:38 -0800

    update about

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({posts, tags}) {
     <>
     <div className="flex flex-col">
       <Meta title="dalton's site" description="Cultivating curiosity" />
-      <h3 className="text-3xl md:text-4xl text-[#0C2059] font-extrabold mb-5 italic">Hi, I'm Dalton. By <span className='not-italic'>☀️</span> - I'm a video editor and designer at Farnam Street. By <span className='not-italic'>🌑</span> - I read and write about history's most interesting people and ideas.</h3>
+      <h3 className="text-3xl md:text-4xl text-gray-900 font-extrabold mb-5 italic">Hi, I'm Dalton. By <span className='not-italic'>☀️</span> - I'm a video editor and designer at Farnam Street. By <span className='not-italic'>🌑</span> - I read and write about history's most interesting people and ideas.</h3>
       <div className='font-serif mb-5 text-stone-800/70'>Every Friday, I share five things from history that will help you live a more deliberate and curious life. Read previous editions <Link href="/tags/221b"><a className='text-blue-700 hover:text-blue-900 hover:underline'>here</a></Link>. Subscribe below:</div>
       <Subscribe title="Learn 5 new things every Friday" caption="Subscribe to The 221b newsletter to recieve an anthology of ideas from history, science, and philosophy every Friday. It's written by yours truly." />
     </div>
