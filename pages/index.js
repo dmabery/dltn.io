c973a444fee554a69ce1d235a9fd345bfbc0dfae
daltonmabery <maberydalton@gmail.com>
2023-01-28 09:59:13 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({posts, tags}) {
     <>
     <div className="flex flex-col ">
       <Meta title="dalton's site" description="Cultivating curiosity" />
-      <h3 className="text-4xl md:text-5xl text-gray-900 font-extrabold mb-2 italic">Hi, I'm Dalton.</h3>
+      <h3 className="text-4xl md:text-5xl text-gray-900 font-extrabold mb-2 italic">Hi, I'm Dalton</h3>
       <ul className='font-body text-sm md:text-base'>
         <li>By ☀️ I'm the digital creator and designer at Farnam Street.</li>
         <li className='mb-5'>By 🌑 I study history, science, and programming and write about what I learn.</li>
