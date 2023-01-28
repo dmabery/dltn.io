8a687013b0b81a02cccc58205a5171cf37893d04
daltonmabery <maberydalton@gmail.com>
2023-01-27 17:25:30 -0800

    c

--- components/Nav.js
+++ components/Nav.js
@@ -4,33 +4,33 @@ import LinkText from './LinkText';
 
 const links = [
     {
-        title: '.writing',
+        title: 'writing',
         slug: 'writing'
     },
     {
-        title: '.notes',
+        title: 'notes',
         slug: 'notes',
     },
     {
-        title: '.newsletter',
+        title: 'newsletter',
         slug: '221b'
     },
     {
-        title: '.about',
+        title: 'about',
         slug: 'about'
     }
 ]
 
 const Nav = () => {
     return (
-        <nav className="flex flex-col md:flex-row font-mono mb-10 mt-10 justify-between items-center">
+        <nav className="flex flex-col md:flex-row justify-between items-center">
             <div className="flex items-center mb-2">
                 <Image src="/ezgif.com-gif-maker.gif" alt="space" width={70} height={70}/>            
                 <div className="font-bold text-xl"><Link href="/"><a className='text-zinc-600/80'>dltn.io</a></Link></div>
             </div>
             <div className="flex row space-x-3">
             {links.map(link => (
-                <LinkText slug={`/${link.slug}`} text={`${link.title}()`} key={link.title} type="nav" />
+                <LinkText slug={`/${link.slug}`} text={`${link.title}`} key={link.title} type="nav" />
             ))}
             </div>
         </nav>
