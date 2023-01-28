aeb09be4e88a09b66e61664149bc6ebc0e9d749c
daltonmabery <maberydalton@gmail.com>
2023-01-27 18:21:25 -0800

    m

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -4,10 +4,10 @@ function PageTitle({ title, description }) {
   return (
     <div className='flex-row items-center mb-3'>
       <div className="font-serif">
-        <h2 className="text-3xl text-gray-900/80 font-bold mb-2">
+        <h2 className="text-4xl md:text-5xl text-gray-900 font-extrabold mb-2 italic">
           {title}
         </h2>
-        <div className="md:text-md prose max-w-screen-md prose:ml-0 underline-offset-2 text-gray-600 prose-a:text-blue-400 prose-a:no-underline hover:prose-a:underline hover:prose-a:text-amber-400 prose-h1:leading-tight prose-h2:text-yellow-300 prose-h3:text-neutral-100 prose-h4:text-neutral-100 sm:prose-h1:leading-tight prose-blockquote:font-normal prose-blockquote:text-neutral-300 prose-blockquote:border-red-500 prose-blockquote:leading-normal prose-blockquote:pl-5 prose-blockquote:not-italic">
+        <div className="font-body text-sm md:text-base">
           <Fragment>{description}</Fragment>
         </div>
       </div>
--- pages/notes.js
+++ pages/notes.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from 'next/image'
 import Link from 'next/link'
+import { Fragment } from 'react'
 import Meta from '../components/Meta'
 import PageTitle from '../components/PageTitle'
 import { getAllByType } from './api/notion'
@@ -19,10 +20,19 @@ export const getStaticProps = async () => {
 const NoteList = ({ posts }) => {
     return (
       <>
-      <Meta title="Notes" description="Everything I've learned that's worth learning has come from a book or other form of medium. These are those notes. Important: These are not summaries. They are notes from books, articles, speeches, or podcasts that was interesting to me and I knew I'd want to revisit." />
+      <Meta title="Book Notes" description="Notes, summaries, and lessons from 30+ books." />
       <PageTitle 
-        title="All Notes"
-        description="These are my book notes (emphasis on mine). Some have a lot of practical insights. Others are full of quotes I liked. It differs from book to book."/>
+        title="Book Notes"
+        description= {
+          <Fragment>
+            <div className='prose mb-5 md:text-base text-sm'>
+              <blockquote>
+              The more that you read, the more things you will know. The more that you learn, the more places you’ll go. – Dr. Seuss
+              </blockquote>
+            </div>
+          </Fragment>
+        }
+      />
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 grid-row gap-4 content-center justify-items-center">
             {posts.map((post) => (
               <Link href={'/posts/' + post.slug} passHref key={post.id}>
