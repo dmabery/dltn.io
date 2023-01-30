cd29c6fd846cd8bd6013f6c19764b523d840e7e2
daltonmabery <maberydalton@gmail.com>
2023-01-29 22:26:43 -0800

    Update LinkedTagButton.js

--- components/LinkedTagButton.js
+++ components/LinkedTagButton.js
@@ -1,6 +1,6 @@
 const LinkedTagButton = ({ btnText }) => {
     return (
-        <span className=" text-blue-600 grow-0 flex-none inline text-xs px-2 rounded-lg py-0.5 mr-1 transition-all">{btnText}</span>
+        <span className=" text-blue-600 border border-gray-300/60 grow-0 flex-none inline text-[10px] px-2 rounded-lg py-1 mr-1 transition-all">{btnText}</span>
     )
 };
 
