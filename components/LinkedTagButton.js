f87927014ae70a20b9b5bae45d5398e88203196e
daltonmabery <maberydalton@gmail.com>
2023-01-30 16:06:45 -0800

    tailwind prettier

--- components/LinkedTagButton.js
+++ components/LinkedTagButton.js
@@ -1,5 +1,5 @@
 const LinkedTagButton = ({ btnText }) => (
-  <span className=" text-blue-600 border border-gray-300/60 grow-0 flex-none inline text-[10px] px-2 rounded-lg py-1 mr-1 transition-all">
+  <span className="text-blue-600 border border-gray-300/60 grow-0 flex-none inline text-[10px] px-2 rounded-lg py-1 mr-1 transition-all">
     {btnText}
   </span>
 );
