dc637766b8d4347e3a75b0a70aab66182245c7f4
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2023-02-01 19:22:24 -0800

    Update SideNote.js

--- components/SideNote.js
+++ components/SideNote.js
@@ -1,12 +1,12 @@
 const SideNote = (props) => (
-  <div className="mt-8 mb-5 px-10 py-5 border-blue-700 border-l-4 rounded-r-md text-slate-700 bg-secondaryBlue">
+  <aside className="mt-8 mb-5 px-10 py-5 border-blue-700 border-l-4 rounded-r-md text-slate-700 bg-secondaryBlue">
     <div className="font-bold text-md mb-2">
       {props.title}
     </div>
     <div className="leading-md leading-normal">
       {props.content}
     </div>
-  </div>
+  </aside>
 );
 
 export default SideNote;
