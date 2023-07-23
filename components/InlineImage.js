24ea215eaeac9108f0ff397b4d51b1baa9f2a3af
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2023-07-23 16:48:49 -0700

    update inline image

--- components/InlineImage.js
+++ components/InlineImage.js
@@ -2,9 +2,7 @@ import Image from "next/image";
 
 const InlineImage = (props) => (
   <div className="float-right ml-3 mb-3">
-    <a href={props.url} target="_blank">
-      <Image src={props.src} height={300} width={200} />
-    </a>
+    <Image src={props.src} height={300} width={200} />
   </div>
 );
 
