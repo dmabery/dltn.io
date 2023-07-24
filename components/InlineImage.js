c3147dca26127a29fc080383c24ed70365a9a845
daltonmabery <maberydalton@gmail.com>
2023-07-23 17:24:15 -0700

    fix inlineimage height

--- components/InlineImage.js
+++ components/InlineImage.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 const InlineImage = (props) => (
   <div className="float-right ml-3 mb-3">
     <a href={props.url} target="_blank">
-      <Image src={props.src} height={300} width={200} />
+      <img className="h-48 object-scale-down md:h-72" src={props.src} />
     </a>
   </div>
 );
