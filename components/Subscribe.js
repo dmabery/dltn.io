a879a777a68bfc62256ec68162def2c938050062
daltonmabery <maberydalton@gmail.com>
2023-09-06 18:54:06 -0700

    Update Subscribe.js

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -32,12 +32,12 @@ const Subscribe = ({ title, placeholder }) => {
   });
 
   const inputClass = classNames({
-    "text-sm appearance-none rounded bg-white w-full text-gray-900/80 mr-1 py-2 px-2 leading-tight focus:border-gray-900": true,
+    "text-sm appearance-none rounded border border-slate-500 w-full text-gray-900/80 mr-1 py-2 px-2 leading-tight focus:border-gray-900": true,
     "opacity-100 cursor-not-allowed": isLoading,
   });
 
   const btnClass = classNames({
-    "flex-shrink-0 bg-gray-900 hover:bg-gray-900/60 text-sm text-white py-2 px-4 rounded transition-all": true,
+    "flex-shrink-0 bg-slate-800 hover:bg-gray-900/60 text-sm text-white py-2 px-4 rounded transition-all": true,
     "opacity-50 cursor-not-allowed": isLoading,
   });
 
