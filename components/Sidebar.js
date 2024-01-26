dc18e737b187346e7921d04303a282871fff9930
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2024-01-25 19:54:43 -0800

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,7 +6,7 @@ export default function Sidebar({ children, left, post }) {
     <>
       <div className="flex max-w-[900px] gap-5 divide-x">
         <div className="w-[100%] md:w-[75%]">{children}</div>
-        <div className="mb-5 hidden w-1/3 p-5 text-sm md:flex md:flex-col">
+        <div className="mb-5 hidden w-1/3 pl-5 text-sm md:flex md:flex-col">
           <h2 className="mb-3 border-b border-[#191919] pb-3 font-sansSerif text-lg font-bold text-gray-900">
             Words on words, books, people, and ideas.
           </h2>
@@ -14,14 +14,14 @@ export default function Sidebar({ children, left, post }) {
             Hi, I'm Dalton
           </h2>
           <div className=" mb-3 border-b border-[#191919] pb-3 text-gray-900/90">
-            <div className="mb-3 border-b border-[#191919] pb-3">
+            <div className="mb-3 border-b border-[#191919] pb-3 text-[16px]">
               I'm a <b>video editor</b> and <b>designer</b> who reads and writes
               about what I learn.
             </div>
             <h2 className="mb-2 font-sansSerif text-lg font-bold text-gray-900">
               Subscribe
             </h2>
-            <div className="mb-3">
+            <div className="mb-3 text-[16px]">
               Every Friday, I send an email with insights, stories, and other
               interesting ideas I learn from reading books. Read previous
               editions
@@ -32,21 +32,11 @@ export default function Sidebar({ children, left, post }) {
             </div>
             <Subscribe />
           </div>
-          <h2 className="mb-3 font-sansSerif text-lg font-bold text-gray-900">
-            Quick Links
-          </h2>
-          <ul className="text-blue-600">
+
+          <ul className="text-[16px] text-blue-600">
             <li>
               <a href="https://twitter.com/dltnio" target="_blank">
                 Follow me on Twitter
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://thewanderingreader.substack.com/"
-                target="_blank"
-              >
-                Read my newsletter
               </a>
             </li>
           </ul>
