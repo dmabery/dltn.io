2b3169f0ef441e93b708ab3b09d466d236bab7a3
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2024-01-27 16:04:02 -0800

    ~

--- components/HomePagePostDisplay.js
+++ components/HomePagePostDisplay.js
@@ -20,11 +20,9 @@ const HomePagePostDisplay = ({
     <>
       <article className="border border-black bg-white text-neutral-300">
         <div className="mb-5 flex flex-col gap-1 text-gray-900">
-          <Link href={`/posts/${slug}`}>
-            <a className="p-5 font-sansSerif text-[28px] font-bold leading-tight hover:underline">
-              {title || ""}
-            </a>
-          </Link>
+          <div className="p-5 font-sansSerif text-[28px] font-bold leading-tight hover:underline">
+            <Link href={`/posts/${slug}`}>{title || ""}</Link>
+          </div>
           <div className="tags border-t border-b border-black py-3 font-sansSerif text-xs text-gray-900">
             <div className="px-5">
               {date} | Filed under:
