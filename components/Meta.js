0370ebed2153d9459b769435a246c73954005f2c
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2024-01-29 20:02:09 -0800

    ~

--- components/Meta.js
+++ components/Meta.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const Meta = (props) => (
   <Head>
@@ -9,22 +9,10 @@ const Meta = (props) => (
     <meta property="og:description" content={props.description} />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://dltn.io/" />
-    <meta
-      property="og:image"
-      content={
-        props.image ||
-        'https://res.cloudinary.com/dde1q4ekv/image/upload/v1675112289/site_og_image_dseevc.png'
-      }
-    />
+    <meta property="og:image" content={props.image || "styles/og image.png"} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:creator" content="@dltnio" />
-    <meta
-      name="twitter:image"
-      content={
-        props.image ||
-        'https://res.cloudinary.com/dde1q4ekv/image/upload/v1675112289/site_og_image_dseevc.png'
-      }
-    />
+    <meta name="twitter:image" content={props.image || "styles/og image.png"} />
   </Head>
 );
 
