2bd87f29556708d6e48bd1d2c5f0b35f430cc745
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2024-02-14 07:59:41 -0800

    ~

--- components/HomePagePostDisplay.js
+++ components/HomePagePostDisplay.js
@@ -20,7 +20,7 @@ const HomePagePostDisplay = ({
     <>
       <article className="text-[neutral-200]">
         <div className="font-sansSerif">
-          <div className="text-2xl font-medium text-neutral-800">
+          <div className="text-3xl font-medium text-neutral-800">
             {title || ""}
           </div>
           <div className="tags flex gap-2 py-2 text-sm text-[#868686]">
