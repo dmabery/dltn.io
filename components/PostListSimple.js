4444db60a5d589da8b0d091a8ec0f2e6ea69a04d
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2024-04-23 16:29:32 -0700

    ~

--- components/PostListSimple.js
+++ components/PostListSimple.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
 const PostListSimple = (props) => (
-  <div className="items-top flex gap-3">
-    <div className="mb-2 text-xl leading-tight underline hover:no-underline">
+  <div className="items-top flex mb-1">
+    <div className="mb-2 text-xl text-slate-900 underline-offset-2 leading-tight underline hover:no-underline">
       <Link href={`/${props.slug}`} legacyBehavior>{props.title}</Link>
     </div>
   </div>
