ea3600747a48071a8c5e1f9df30d0e8c4eeb0ed6
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2024-07-14 08:46:35 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["images.ctfassets.net", "res.cloudinary.com", "_.cloudinary.com", "evm.heg.mybluehost.me"],
+    domains: [
+      "images.ctfassets.net",
+      "res.cloudinary.com",
+      "_.cloudinary.com",
+      "evm.heg.mybluehost.me",
+      "sqb.jln.mybluehost.me",
+      "i0.wp.com",
+    ],
   },
   async redirects() {
     return [
