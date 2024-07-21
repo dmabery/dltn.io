dbc10525ceb8aebfcbaf7eb8e4126d3988f4043e
Dalton Mabery <61725749+dmabery@users.noreply.github.com>
2024-07-20 18:26:40 -0700

    ~

--- pages/notes.js
+++ pages/notes.js
@@ -1,45 +1,20 @@
 /* eslint-disable react/no-unescaped-entities */
-import fs from "fs";
-import matter from "gray-matter";
 import Image from "next/image";
 import Link from "next/link";
-import path from "path";
 import Meta from "../components/Meta";
-import PageTitle from "../components/PageTitle";
+import { getAllPosts } from "../lib/getMarkdownFiles";
 
 export const getStaticProps = async () => {
-  const files = fs.readdirSync("words/posts");
-  const postsDirectory = "words/posts";
-
-  const posts = files
-    .filter((fileName) => {
-      // Check if the item is a file and not a directory
-      const filePath = path.join(postsDirectory, fileName);
-      return fs.statSync(filePath).isFile();
-    })
-    .map((fileName) => {
-      const slug = fileName.replace(".md", "");
-      const readFile = fs.readFileSync(
-        path.join(postsDirectory, fileName),
-        "utf-8"
-      );
-      const { data: frontmatter, content } = matter(readFile);
-
-      return {
-        slug,
-        frontmatter,
-        content,
-      };
-    });
+  const posts = await getAllPosts();
 
   return {
-    props: { posts },
+    props: {posts}
   };
 };
 
 const NoteList = ({ posts, frontmatter }) => {
   const sortedPosts = posts.sort(
-    (a, b) => new Date(b.frontmatter.Date) - new Date(a.frontmatter.Date)
+    (a, b) => new Date(b.date) - new Date(a.date)
   );
 
   return <>
@@ -47,18 +22,19 @@ const NoteList = ({ posts, frontmatter }) => {
       title="Book Notes"
       description="Notes, summaries, and lessons from 30+ books."
     />
-    <PageTitle
-      title="Book Notes"
-    />
-    <div className="">
-      <div className="mt-7 grid grid-cols-3 gap-3">
+    <div>
+      <div className="grid grid-cols-2 gap-5 md:grid-cols-3">
         {sortedPosts
-          .filter((post) => post.frontmatter.Type === "Book Notes")
+          .filter((post) => post.type === "Book Notes")
           .map((post, index) => (
-            <div className="transition-all hover:scale-105 cursor-pointer" key={post.id}>
-              <Link href={`/posts/${post.slug}`} legacyBehavior>
+            <div className="cursor-pointer transition-all hover:scale-105">
+              <Link
+                href={`/posts/${post.slug}`}
+                className="transition-all hover:scale-105"
+                legacyBehavior>
                 <Image
-                  src={post.frontmatter.Image}
+                  key={post.id}
+                  src={post.image}
                   layout="responsive"
                   height={75}
                   width={50}
