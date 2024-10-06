a696c74df70a0c32973c7043b3ad4e8b43f3c801
daltonmabery <maberydalton@gmail.com>
2024-10-06 11:01:29 -0700

    ~

--- components/PostListSimple.js
+++ components/PostListSimple.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
 const PostListSimple = (props) => (
-  <div className="mb-2 gap-3 flex items-center text-md font-sansSerif">
-    <div className="hidden shrink-0 text-gray-600 md:inline">
-      {props.date.slice(0,4)} <span className="text-gray-900/50 text-sm">•</span> {props.date.slice(5,7)}
+  <div className="mb-3 gap-4 flex items-center text-md font-sansSerif">
+    <div className="hidden shrink-0 text-neutral-200/90 md:inline">
+      {props.date.slice(0,4)} <span className="text-sm">•</span> {props.date.slice(5,7)}
     </div>
     <div className="underline hover:text-blue">
       <Link href={`/${props.slug}`}>{props.title}</Link>
