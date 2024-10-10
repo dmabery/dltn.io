a71895f5ff76cb7839d17f48577da265aee49dbe
daltonmabery <maberydalton@gmail.com>
2024-10-09 19:41:07 -0700

    ~

--- components/PostBodyContent.js
+++ components/PostBodyContent.js
@@ -6,7 +6,7 @@ import Subscribe from "./Subscribe";
 import SubscribeGroup from "./SubscribeGroup";
 
 const PostBodyContent = (props) => (
-  <div className="prose-invert prose-a:font-normal prose prose-quoteless prose-h1:text-2xl prose-h1:font-medium prose-h1:leading-tight prose-h2:text-xl prose-h2:font-medium prose-h3:text-xl prose-h3:font-medium prose-h4:text-lg prose-h4:font-medium hover:prose-a:text-blue-600 hover:prose-a:no-underline prose-a:text-blue-500 prose-blockquote:py-0 prose-blockquote:border-white prose-blockquote:border-l-[2px] prose-blockquote:font-normal prose-blockquote:not-italic prose-blockquote:text-neutral-200 prose-pre:font-normal prose-img:m-0 sm:prose-h1:leading-tight">
+  <div className="prose-invert leading-normal prose-a:font-normal prose prose-quoteless prose-h1:text-2xl prose-h1:font-medium prose-h1:leading-tight prose-h2:text-xl prose-h2:font-medium prose-h3:text-xl prose-h3:font-medium prose-h4:text-lg prose-h4:font-medium hover:prose-a:text-blue-600 hover:prose-a:no-underline prose-a:text-blue-500 prose-blockquote:py-0 prose-blockquote:border-white prose-blockquote:border-l-[2px] prose-blockquote:font-normal prose-blockquote:not-italic prose-blockquote:text-neutral-200 prose-pre:font-normal prose-img:m-0 sm:prose-h1:leading-tight">
     <Markdown
       options={{
         overrides: {
--- pages/_app.js
+++ pages/_app.js
@@ -11,23 +11,19 @@ const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-white md:px-10 px-0">
-      <nav className="py-3 px-0 md:px-10 ml-0 mb-10">
+    <div className="px-0 mx-auto max-w-[610px]">
+      <nav className="py-3 mb-10">
         <Nav/>
       </nav>
-      <div className="md:p-10 max-w-[650px]">
       <Head>
         <link rel="icon" href="/favicon 3.png" />
       </Head>
-      <div className="p-3 md:p-0">
         <main className="mb-auto min-h-screen pb-32">
           <QueryClientProvider client={queryClient}>
             <Component {...pageProps} />
             <Analytics />
           </QueryClientProvider>
         </main>
-        </div>
-      </div>
     </div>
   );
 }
