4e74bce7d00a6c210d0fee17abf3f94b609b7ceb
daltonmabery <maberydalton@gmail.com>
2024-10-13 18:52:44 -0700

    ~

--- pages/notes.js
+++ pages/notes.js
@@ -24,9 +24,9 @@ const NoteList = ({ posts }) => {
     />
     <div>
       <div>
-        <h1 className="text-2xl mb-2">Notes, Quotes, and Lessons from Books, Articles, and PDFs</h1>
-        <p className="mb-3">These are not—nor do they intend to be—summaries.</p>
-        <p className="mb-3">Enjoy the "highlights," but don't supplement reading these for reading the actual source. If it's posted, I highly reccomend it.</p>
+        <h1 className="text-2xl mb-2">Notes, quotes, and lessons from books</h1>
+        <p className="mb-3">These are not—nor do they intend to be—summaries. These are my highlights and notes, the things that I enjoyed the most from the book.</p>
+        <p className="mb-3">Don't supplement reading these notes for reading the actual book. If it's posted, I highly recommend reading it. I don't have a rating system because if the book wasn't at least a 7/10, I wouldn't take notes on it.</p>
         <p className="mb-5">Sign up below for an email when I post new notes.</p>
         <div className="border-b mb-10 pb-10">
           <Subscribe />
@@ -52,7 +52,7 @@ const NoteList = ({ posts }) => {
               </div>
               <div>
                 <Link href={`/posts/${post.slug}`} className="md:text-xl hover:text-blue-900 font-semibold">{post.title}</Link>
-                <p className="text-sm mt-1 md:text-base md:mt-3"><Markdown>{`${post.excerpt.slice(0,200)}...`}</Markdown></p>
+                <p className="text-sm mt-1 md:text-base md:mt-3"><Markdown>{`${post.excerpt.slice(0,500)}...`}</Markdown></p>
               </div>
             </div>
           ))}
