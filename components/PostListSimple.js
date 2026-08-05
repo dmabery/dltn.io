640a67367550834ac587346075622baa27931ce8
daltonmabery <maberydalton@gmail.com>
2026-08-05 16:59:34 -0700

    ~

--- components/PostListSimple.js
+++ components/PostListSimple.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const PostListSimple = (props) => (
-  <div className="mb-2 gap-3 flex items-center text-md font-sansSerif">
+  <div className="mb-2 gap-3 flex items-center text-md">
     <div className="hidden w-[100px] min-w-[55px] shrink-0 text-gray-600 md:inline">
       {props.date}
     </div>
